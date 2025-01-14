d2eeb8eeab7c17024847dbe5031e8482fecf3995
Shrihari123456 <amruthurshrihari@gmail.com>
2025-01-15 00:47:58 +0530

    issues model added

--- backend/constants.js
+++ backend/constants.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const DB_NAME= "mindfull"
-=======
 export const DB_NAME= 'mindfull'
->>>>>>> dd7b515eeab5eda9a6661d9432ff388bf3de8696
--- backend/index.js
+++ backend/index.js
@@ -6,16 +6,6 @@ dotenv.config({
 })
 
 connectDB()
-<<<<<<< HEAD
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log("MONGO db connection failed !!! ", err);
-// })
-=======
 /*.then(() => {
     app.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
@@ -24,4 +14,3 @@ connectDB()
 .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
 })*/
->>>>>>> dd7b515eeab5eda9a6661d9432ff388bf3de8696
