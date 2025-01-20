d65fe97b378eeb1c8d9203b6f10cfa2536e498ba
Shrihari123456 <amruthurshrihari@gmail.com>
2025-01-20 15:32:23 +0530

    parent router

--- backend/src/routes/parent.routes.js
+++ backend/src/routes/parent.routes.js
@@ -3,7 +3,10 @@ import { parent_verifyJWT } from "../middleware/auth.middleware.js";
 import {
   registerParent,
   loginParent,
-  logoutParent
+  logoutParent,
+  getSessions,
+  getJournals,
+  getIssues
 } from "../controllers/parent.controller.js";
 
 const router = express.Router();
@@ -13,5 +16,8 @@ router.get("/parent/:parentId/report", getStudentReport);
 router.post("/login", parent_verifyJWT,loginParent);
 
 router.post("/logout", parent_verifyJWT, logoutParent);
+router.get("/parent/:parentId/sessions", getSessions);
+router.get("/parent/:parentId/journals", getJournals);
+router.get("/parent/:parentId/issues", getIssues);
 
 export default router;
