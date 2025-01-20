69a49668bdc64cda9675efc2e7e8578b9458d608
Vaibhav M N <vaibhavvaibhu2005@gmail.com>
2025-01-20 13:37:25 +0530

    Update resource.model.js

--- backend/src/models/resource.model.js
+++ backend/src/models/resource.model.js
@@ -1,38 +1,43 @@
 import {Schema, model} from "mongoose";
 
 const resourceSchema = new Schema({
-    title: {
-        type: String,
-        required:true
-    },
-    type: {
-        type: String, enum :[ 'book', 'video', 'blog', 'podcast', 'event'], required: true 
-    },
-    content: {
-        type: String,
-        required: true
-    },
-    user:{
-        type: Schema.Types.ObjectId, ref: 'User', required: true
-    },
-    referenceId: { type: String, required: true },
-    watched: { type: Boolean, default: false },
-    related_interests: [{
-        type: Schema.Types. ObjectId,
-        ref: "Interest"
-    }],
-    related_issues: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Issue",
-      }], // Links related issues
-      related_goals: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Interest",
-      }], 
-      createdAt: { 
-        type: Date, 
-        default: Date.now 
-      },
-})
+  url: String,
+  type: {
+    type: String,
+    enum: ['book', 'video', 'blog', 'podcast', 'event'],
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  content: String,
+  related_interest: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Interest'
+  }],
+  related_issues: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Issue'
+  }],
+  related_goals: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Interest'
+  }],
+  watched: {
+    type: Boolean,
+    default: false
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  metadata: {
+    type: Map,
+    of: String
+  }
+},{
+  timestamps: true
+});
 
 export const Resource= model("Resource",resourceSchema)
