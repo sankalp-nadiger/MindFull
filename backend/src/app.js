7703accd61c1757072056172915dc984ba86a7cc
sankalp nadiger <nadigersankalp@gmail.com>
2025-02-17 23:59:21 +0530

    solved cors issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173', // Adjust the frontend URL as needed
+    origin: '*', // Adjust the frontend URL as needed
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
 }));
