f0d78ff09ef81fe4375c905e8334ec4c8415a989
sankalp nadiger <nadigersankalp@gmail.com>
2025-02-18 22:06:17 +0530

    socket env fixed

--- frontend/src/components/Councellor/Session.jsx
+++ frontend/src/components/Councellor/Session.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import { Bell, Calendar, Users, Settings, LogOut, Video } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
-const socket = io("http://localhost:8000", {
+const socket = io(`${import.meta.env.VITE_BASE_API_URL}`, {
   transports: ["websocket"],
   withCredentials: true
 });
--- frontend/src/components/Videochat/Videochat.jsx
+++ frontend/src/components/Videochat/Videochat.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { io } from 'socket.io-client';
 import { motion } from 'framer-motion';
 
-const socket = io('http://localhost:8000', {
+const socket = io(`${import.meta.env.VITE_BASE_URL}`, {
   transports: ['websocket'],
   withCredentials: true,
 });
